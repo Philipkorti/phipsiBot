4a816e570050384793c08312832d905c17ae8513
philipKorti <philip.korthauer@gmx.at>
2024-09-12 18:38:35 +0200

    add Entity

---
+++ DbConnection/Entity/Services.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbConnection.Entity
+{
+    public class Services
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Status { get; set; }
+
+        public string? ChannelId { get; set; } 
+    }
+}
